801e438a0e0fbe0eaabe7ef0dc69fcf85a59302d
FAhmedB <bouafif.ahmed@gmail.com>
2016-04-27 15:46:54 +0100

    testing the hello function

---
+++ TestHello.m
@@ -0,0 +1,12 @@
+function  TestHello(  )
+%TESTHELLO Summary of this function goes here
+%   Detailed explanation goes here
+
+if Hello_World(2) == 3 
+    disp('SUCCESS');
+else
+    disp('FAILURE');
+end
+
+end
+
