5d2a002335bccf597970f2a4d981eccafb275c9e
FAhmedB <bouafif.ahmed@gmail.com>
2016-05-11 12:59:17 +0100

    The function returns a scalar not a matrix

--- BinaryError/berror.m
+++ BinaryError/berror.m
@@ -2,7 +2,8 @@ function [ error ] = berror( A, B )
 %BERROR Summary of this function goes here
 %   Detailed explanation goes here
 
-error = sum(xor(A,B));
+errorM = xor(A,B);
+error = sum(errorM(:));
 
 end
 
