e6f3e45aa22c33e95938b7515cc6384b79d83dc2
FAhmedB <bouafif.ahmed@gmail.com>
2016-05-11 13:00:00 +0100

    Excluding non rectangular symmetries

--- BlockTransformation/isometry.m
+++ BlockTransformation/isometry.m
@@ -10,15 +10,7 @@ switch isometryNumber
     case 2
         outputBlock = flipud(block);
     case 3
-        outputBlock = block';
-    case 4
-        outputBlock = rot90(block',2);
-    case 5
-        outputBlock = rot90(block);
-    case 6
         outputBlock = rot90(block,2);
-    case 7
-        outputBlock = rot90(block,-1);
 end
 
 end
